de16f7309abb8af7777377e162d951688248cec3
alamhadia27-design <alamhadia27@gmail.com>
2025-09-09 16:46:08 +0000

    container config done

---
+++ 01_basics/datatype.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 +3) // this will give you error it's result will found out to the browser
+//  not at console space we are using node.js not browser
+
+console.log(3+3) // code readability should be high
+
+let name="Hadia"
+let age=20
+let islogedin=false
+
+//number => 2 power of 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => no value
+//symbol => find uniqueness
+
+
+//object
+
+
+console.log(typeof "Hadia");
+
+console.log(typeof null); //object type
+console.log(typeof undefined); //undefined type
