a5f5be78d3da98e02821f3079b5579eaf30213ba
alamhadia27-design <alamhadia27@gmail.com>
2025-09-09 17:07:30 +0000

    container config done

---
+++ 01_basics/convertionoperation.js
@@ -0,0 +1,29 @@
+let score = "33abc"
+
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+//null => 0
+
+let islogedin= 1
+
+let booleanislogedin = Boolean(islogedin)
+//console.log(booleanislogedin);
+
+
+// 1 => true , 0=> false
+// "" => false , string => true
+
+let someNumber =44
+
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
