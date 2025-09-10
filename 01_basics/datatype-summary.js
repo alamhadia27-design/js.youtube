78c5afd5a500054ae96ee03ebc1b8371bbc932c3
alamhadia27-design <alamhadia27@gmail.com>
2025-09-10 11:15:03 +0000

    container config done

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,31 @@
+// Permitive  (call by value)
+
+// 7 Types (String, Number, BigInt, Symbol, Boolean, Null, Undefined)
+
+//Non-Permitive (Reference) (call by reference)
+
+// 3 Types (Object, Function, Array)
+
+// For Declaring Symbol
+
+const id= Symbol('1214')
+const anotherid= Symbol('1214')
+
+console.log(id == anotherid);
+
+//For Declaring BigInt
+
+const bignum=Number(12257635558n)
+
+console.log(bignum);
+
+const nothing = null
+
+console.log(typeof nothing)
+
+/******************** Non Permitive *********************/
+const rolemodel = ['Hazrat-Muhammad', 'Hazrat-Ali']
+
+console.log(typeof rolemodel);
+
+console.log(typeof bignum)
