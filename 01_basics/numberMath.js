f9e8a74cfd256914c6791ffb7a9a6dd5e3b6c7f6
alamhadia27-design <alamhadia27@gmail.com>
2025-09-10 16:01:34 +0000

    container config done

---
+++ 01_basics/numberMath.js
@@ -0,0 +1,46 @@
+const score=400
+
+const balance= new Number(100)
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString().length);
+
+// console.log(typeof balance);
+
+// console.log(balance.toFixed(2));
+
+const othernum = 23.8966
+
+// console.log(othernum.toPrecision(3));
+
+const othernumber = 123.8900
+
+//console.log(othernumber.toPrecision(3));
+
+const hundreds = 1000000000
+
+//console.log(hundreds.toLocaleString())
+
+
+
+/****************** Maths *******************/
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.3))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.3))
+// console.log(Math.min(4,3,1,7))
+// console.log(Math.max(4,3,1,7))
+
+console.log(Math.random())
+
+console.log(Math.random()*10)
+
+console.log((Math.random()*10) +1)
+
+const min = 5
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min +1)) +1)
