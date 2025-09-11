065bdeafb6b0541437c17e5629f8b6f37c9d6f41
alamhadia27-design <alamhadia27@gmail.com>
2025-09-11 06:04:45 +0000

    container config done

---
+++ 02_basics/array.js
@@ -0,0 +1,43 @@
+// array
+
+const myarr = [0,1,2,3,4,5]
+
+const myarr2 = ['LHR','ISL','Peshawar']
+
+const myarr3 = new Array(5,6,7,8)
+
+// console.log(myarr2);
+// console.log(myarr3);
+// console.log(myarr[4]);
+
+//Array Methods
+
+// myarr.push(6)
+// myarr.push(7)
+// myarr.pop()
+
+// myarr.unshift(9)
+// myarr.shift()
+
+// console.log(myarr.includes(9));
+// console.log(myarr.indexOf(9));
+
+// const newArray = myarr.join() // Adda all the elements  of an array into a string, separated by specific separator
+// console.log(myarr);
+// console.log(newArray);
+// console.log(typeof newArray);
+
+// slice, splice
+
+console.log("A", myarr);
+
+// const myn1= myarr.slice(1,3) //returns a copy of section an array
+
+// console.log(myn1);
+// console.log("B", myarr);
+
+
+//const myn2= myarr.splice(1,3)// removing elements from an array
+// console.log(myn2);
+// console.log("x", myarr);
+
