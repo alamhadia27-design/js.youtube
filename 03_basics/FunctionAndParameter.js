29942c01c77f87db61769769a3dd3cd78baebe25
alamhadia27-design <alamhadia27@gmail.com>
2025-09-11 13:09:05 +0000

    container config done

---
+++ 03_basics/FunctionAndParameter.js
@@ -0,0 +1,32 @@
+
+function sayname(){
+    console.log("H");
+    console.log("A");
+    console.log("D");
+    console.log("I");
+    console.log("A");
+
+}
+
+// sayname()
+// defining a function is called parameter
+// function addition(num1,num2){
+//     console.log(num1+num2);
+    
+// }
+
+// // calling a function is arguments
+// addition(3,4)
+// const result =addition(3,4)
+// console.log("Results is = ", result);
+
+
+function addition(num1,num2){
+    let result= num1 + num2
+   // console.log("Hadia");
+    return result
+}
+
+const result =addition(3,4)
+
+console.log("Results is = ", result);
